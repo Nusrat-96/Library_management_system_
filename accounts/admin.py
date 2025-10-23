0129454f63e7e09bc54533b2d6fee01a897b1ea1
nusrat <nusratadiba88@gmail.com>
2025-10-23 13:36:04 +0600

    feat: Add pages apps

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,52 @@
+"""#accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+print("-----------------Is in adming -accounts -----------")
+
+CustomUser = get_user_model()
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+    "email",
+    "username",
+    "is_superuser",
+    ]
+admin.site.register(CustomUser, CustomUserAdmin)
+"""
+
+# accounts/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+CustomUser = get_user_model()
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("email", "username", "is_superuser")
+
+
+    #Must have to add fieldsets and add_fieldsets
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+    search_fields = ("email", "username")
+    ordering = ("email",)
